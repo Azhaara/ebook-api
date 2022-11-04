dd4e8b99ea571fbc0ac66916adcee9bd3173782c
Azhaara <azhaaraara@gmail.com>
2022-11-04 18:17:20 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\AuthController;
 
 
 
@@ -36,5 +37,23 @@ Route::get('halo', function(){
 // Route::get('siswa', [SiswaController::class, 'destroy']);
 
 
-Route::apiresource('siswa', SiswaController::class);
-Route::apiresource('books', BookController::class);
+//Route::apiresource('siswa', SiswaController::class);
+//Route::apiresource('books', BookController::class);
+
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('siswa', SiswaController::class)->except('create', 'edit', 'show', 'index');
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
